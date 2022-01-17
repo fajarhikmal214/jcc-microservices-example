035d38810c5f880c8512064d0aba600d922771a7
Fajar Hikmal <fajarhikmal214@gmail.com>
2022-01-17 19:39:25 +0700

    feat(ticket): Add ticket modules

---
+++ ticket-service/middlewares/validations/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    createEventTicketValidation: require('./create-event-ticket'),
+    eventIdParamValidation: require("./event-id-param"),
+    eventTicketIdParamValidation: require("./event-ticket-id-param"),
+  };
+  
